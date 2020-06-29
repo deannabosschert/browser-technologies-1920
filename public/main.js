8bf7f719656afddc4ecd642cf2b1d5578d390974
deannabosschert <deanna.bosschert@gmail.com>
2020-06-29 17:30:23 +0200

    refactor: delete unused lines

--- public/main.js
+++ public/main.js
@@ -1,44 +1,20 @@
+document.getElementById('datePicker').valueAsDate = new Date();
+
+var date = new Date();
+var currentTime = date.getHours() + ':' + date.getMinutes();
+document.getElementById('currentTime').value = currentTime;
+
+// source: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
 function jsAvailable() {
   storageAvailable()
 }
-
-
-// saveButton.addEventListener('click', function(event) {
-//   console.log("op button geklikt")
-//   window.localStorage.setItem("tables", JSON.stringify(doseTables))
-//   console.log("er zit nu dit in:")
-//   console.log(doseTables)
-// })
-// For compatibility, a non-function object with a `handleEvent` property is
-// treated just the same as a function itself.
-// buttonElement.addEventListener("click", function(event) {
-//   console.log("op button geklikt")
-//   console.log(doseTables)
-//   localStorage.setItem("tables", JSON.stringify(doseTables))
-// });
-
-// function storageAvailable() {
-//   if (storageAvailable('localStorage')) {
-//     if (localStorage.getItem("tables") === null) {
-//       console.log("niets in je storage")
-//     } else {
-//       console.log("er zit data in je localStorage 🤓")
-//       const tables = JSON.parse(localStorage.getItem("tables"))
-//       doseTables.push(tables)
-//     }
-//
-//   } else {
-//     // Too bad, no localStorage for us.
-//   }
-// }
 
 if (storageAvailable('localStorage')) {
   console.log('Yay! We can use localStorage')
   var loadButton = document.querySelector('#load')
   var tablesElement = document.querySelector('#tablesElement')
-  // var savedDoses = document.getElementById("savedDoses").innerHTML;
-    if (tablesElement.innerHTML == 0) {
-      console.log("geen data te zien")
+  if (tablesElement.innerHTML == 0) {
+    console.log("geen data te zien")
   } else {
     console.log("set tables")
     localStorage.setItem("tables", tablesElement.innerHTML)
@@ -48,28 +24,15 @@ if (storageAvailable('localStorage')) {
     console.log("niets in je storage")
   } else {
     console.log("er zit data in je localStorage 🤓")
-    // if (tablesElement.innerHTML === null) {
     loadButton.addEventListener('click', function() {
       var savedTables = localStorage.getItem("tables")
       tablesElement.innerHTML = savedTables
     })
-
-    // }
   }
-
-
-
-  // load tables
-  // console.log('tables laden')
-  // const loadDingen = window.localStorage.getItem('doseTables')
-  // tablesElement.innerHTML = doseTablesSave.innerHTML
-
 } else {
   console.log('Too bad, no localStorage for us')
 }
 
-
-// source: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
 function storageAvailable(type) {
   var storage;
   try {
@@ -94,9 +57,3 @@ function storageAvailable(type) {
   }
 }
 jsAvailable()
-
-document.getElementById('datePicker').valueAsDate = new Date();
-
-var date = new Date();
-var currentTime = date.getHours() + ':' + date.getMinutes();
-document.getElementById('currentTime').value = currentTime;
